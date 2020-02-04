1da2198526fa4304e6f0af9e197af7b9e55a402d
Clyde Mendonca <clyde.mendonca01@gmail.com>
2020-02-04 16:30:26 +0530

    JSP Page successfully run

--- src/main/java/com/pluralsight/EventTrackerApplication.java
+++ src/main/java/com/pluralsight/EventTrackerApplication.java
@@ -2,12 +2,14 @@ package com.pluralsight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class EventTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventTrackerApplication.class, args);
+    }
 
 }
